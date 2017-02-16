b3b8a89ed9ac067962212753161a8c1410dcd461
taurin0723 <hiroya0723h@hotmail.co.jp>
2017-02-17 01:44:55 +0900

    ex3-3

---
+++ .spyproject/Element.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 16 22:13:04 2017
+
+@author: hiroya
+"""
+
+import numpy as np
+
+class Element(object):  
+    def __init__(self,EI,F0,w0,l,N):
+        self.EI = EI    
+        self.F0 = F0
+        self.w0 = w0
+        self.l = l
+        self.N = N
+        
+    def makeKbCblist(self):
+        l = self.l
+        tmp = [[0 for i in range(4)] for j in range(4)]
+        i = 0
+        j = 0
+        tmp[i][j] = 12.0/l**3
+        j = j + 1
+        tmp[i][j] = -6.0/l**2
+        j = j + 1
+        tmp[i][j] = -12/l**3
+        j = j + 1
+        tmp[i][j] = -6/l**2
+        i = i + 1
+        j = 0
+        tmp[i][j] = -6/l**2
+        j = j + 1
+        tmp[i][j] = 4/l
+        j = j + 1
+        tmp[i][j] = 6/l**2
+        j = j + 1
+        tmp[i][j] = 2/l
+        i = i + 1
+        j = 0
+        tmp[i][j] = -12/l**3
+        j = j + 1
+        tmp[i][j] = 6/l**2
+        j = j + 1
+        tmp[i][j] = 12/l**3
+        j = j + 1
+        tmp[i][j] = 6/l**2
+        i = i + 1
+        j = 0
+        tmp[i][j] = -6/l**2
+        j = j + 1
+        tmp[i][j] = 2/l
+        j = j + 1
+        tmp[i][j] = 6/l**2
+        j = j + 1
+        tmp[i][j] = 4/l
+        
+       # w = self.w
+       # i = 0
+        #for  in w:
+        #    print(f)
+        
+        return np.array(tmp)*self.EI
+
+    def checknotfixedNum(self):
+        num = []
+        for i in range(len(self.w0)):
+            if not self.w0[i] == 0:
+                num.append(i)
+        return num
+    
+    
+    def hello(self):
+        print("hello")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
--- .spyproject/main.py
+++ .spyproject/main.py
@@ -5,4 +5,61 @@ Created on Thu Feb 16 21:07:30 2017
 @author: hiroya
 """
 
-print("hello")
+import numpy as np
+from Element import *
+import matplotlib.pyplot as plt
+import time
+
+def mergeMatrix(Mat,N):
+    num = 2*N+2
+    tmpMat = [[0 for i in range(num)] for j in range(num)]
+    i=0    
+    for mat in Mat:
+        for y in range(4):
+            for x in range(4):
+                tmpMat[i*2+y][i*2+x] += mat[y][x]
+        i = i+1     
+    return tmpMat
+    
+def calcW(K,F,w,N):
+    list = checknotfixedNum(w)
+    num = len(list)
+    tmpf = []
+    for i in list:
+        tmpf.append(F[i])
+    f = np.array(tmpf)
+            
+    Mat = [[0 for i in range(num)] for j in range(num)]
+    for i in range(num):
+        for j in range(num):
+            Mat[i][j] = K[list[i]][list[j]]
+    invMat = np.linalg.inv(Mat)
+    return invMat.dot(f)    
+    
+def checknotfixedNum(w):
+    num = []
+    for i in range(len(w)):
+        if not w[i] == 0:
+            num.append(i)
+    return num
+         
+def main():
+    N=1
+    w0 = np.array([0,0,1,1])
+    F0 = np.array([0,0,98,0])
+    EI=100
+    l = 1
+    elt = Element(EI,F0,w0,l,N)
+    K1 = elt.makeKbCblist()
+    elt2 = Element(EI,F0,w0,l,N)
+    K2 = elt.makeKbCblist()
+    elt3 = Element(EI,F0,w0,l,N)
+    K3 = elt.makeKbCblist()
+    elts = [K1]
+    K = mergeMatrix(elts,N)
+    w = (calcW(K,F0,w0,N))
+    print(w)
+    
+if __name__ == '__main__':
+    main()
+    
